b19762d5a8fa18f6f69074155b236820910cf410
Mauro Alves <mauroccalves@outlook.com>
2026-05-26 21:33:46 +0000

    subindo versao formacao

--- client/src/components/AddTask.jsx
+++ client/src/components/AddTask.jsx
@@ -59,7 +59,7 @@ const AddTask = ({ onAdd }) => {
       </div>
       
       <button type="submit" className="btn btn-block success">
-        Adicionar Tarefa (DNS)
+        Adicionar Tarefa Formacao
       </button>
       
       <Modal
